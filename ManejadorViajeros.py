34a37a9eb398cd179038c23586cdfdaaac34a773
EliasRivamar <132520059+EliasRivamar@users.noreply.github.com>
2023-05-05 13:54:49 -0300

    Update ManejadorViajeros.py

--- ManejadorViajeros.py
+++ ManejadorViajeros.py
@@ -1,40 +1,56 @@
-from Viajeros import Viajeros
+from Viajero import Viajero
 import csv
 
-class ManejadorViajeros:
+
+class ManejadorViajero:
     def __init__(self):
-        self.__listaviajeros = []
+        self.__listaviajero = []
 
-    def agregarviajero (self,unviajero):
-        self.__listaviajeros.append(unviajero)
+    def agregarviajero(self, unviajero):
+        self.__listaviajero.append(unviajero)
 
-    def testviajeros(self):
-        archivo=open("viajeros.csv")
-        reader = csv.reader (archivo, delimiter= ",")
+    def testViajeros(self):
+        archivo = open("viajeros.csv")
+        reader = csv.reader(archivo, delimiter=',')
         for fila in reader:
-            num = int(fila[0])
-            dni = fila[1]
+            num = (fila[0])
+            dni = (fila[1])
             nombre = fila[2]
             apellido = fila[3]
-            millas_acum = fila[4]
-            unviajero= Viajeros (num,dni,nombre,apellido,millas_acum)
+            millas_acum = (fila[4])
+            unviajero = Viajero(num, dni, nombre, apellido, millas_acum)
             self.agregarviajero(unviajero)
         archivo.close()
-    
+
     def __str__(self):
         s = ""
-        for viajeros in self.__listaviajeros:
-            s+=str(viajeros)+"\n"
+        for viajero in self.__listaviajero:
+            s = s + str(viajero) + "\n"
         return s
-    
-    def buscarviajeros (self, numero):
-        indice=0
-        valorderetorno=None
-        bandera=False
-        while not bandera and indice < len(self.__listaviajeros):
-            if self.__listaviajeros[indice].getNumeroViajero()==numero:
+
+    def buscarViajero(self, numero):
+        indice = 0
+        valor = None
+        bandera = False
+        while not bandera and indice < len(self.__listaviajero):
+            if self.__listaviajero[indice].getnum() == numero:
                 bandera = True
-                valorderetorno = indice
+                valor = indice
             else:
-                indice= indice + 1
-        return self.__listaviajeros[valorderetorno]
+                indice = indice + 1
+        return self.__listaviajero[valor]
+
+    def mayores(self):
+        maximo = self.__listaviajero[0].getmillas()
+        for Viajero in self.__listaviajero:
+            maximo = Viajero.__ge__(maximo)
+        return maximo
+
+    def mostarMayores(self, maximo):
+        indice = 0
+        p = ""
+        while indice < len(self.__listaviajero):
+            if int(self.__listaviajero[indice].getmillas()) == int(maximo):
+                p = p + str(self.__listaviajero[indice]) + "\n"
+            indice = indice + 1
+        return str(p)
