9fd4c560e14b8259a56386d7c89e59f964665e06
EliasRivamar <132520059+EliasRivamar@users.noreply.github.com>
2023-05-05 13:53:58 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,41 +1,41 @@
-from Viajeros import Viajeros
-from ManejadorViajeros import ManejadorViajeros
+from Viajero import Viajero
+from ManejadorViajero import ManejadorViajero
 
-if __name__=="__main__":
-    mv = ManejadorViajeros()
-    mv.testviajeros()
+
+if __name__ == '__main__':
+    mv = ManejadorViajero()
+    mv.testViajeros()
     print(str(mv))
-    numero = int (input("ingrese el numero de un viajero:"))
+    numero = int(input("Ingrese el numero de un viajero: "))
     while numero != 0:
-       unviajero = mv.buscarviajeros(numero)
-       if unviajero==None:
-            print ('---El viajero ingresado no exite---')
-       else:
-          while numero != 0:
-            print("1: Consutar Cantidad de Millas del Viajero")
-            print("2: Acumular Millas")
-            print("3: Canjear Millas")
-            print("4: Elegir un nuevo viajero")
-            print("5: Acumular Todas las Millas")
-            print("6: Salir")
-            opcion= int (input())
-            if opcion == 1:
-               print (Viajeros.cantidadtotalmillas(unviajero))
-            elif opcion == 2:
-                millas= int(input("ingrese la cantidad de millas acumuladas: "))
-                print (Viajeros.acumularmillas(unviajero,millas))
-            elif opcion == 3:
-                canjear= int(input("ingrese la cantidad de millas a canjear: "))
-                print (Viajeros.canjearmillas(unviajero, canjear))
-            elif opcion==4:
-                numero = 0
-            elif opcion==5:
-                print (Viajeros.__add__(unviajero))
-            elif opcion ==6:
-                print(Viajeros.__sub__(unviajero))
-            elif opcion==6:
-                exit()
-            else:
-                print ("---Opcion Incorrecta---")
-    numero = int(input("ingrese el numero de un viajero"))
-       
+        unviajero = mv.buscarViajero(numero)
+        if unviajero is None:
+            print("----El viajero no existe----")
+        else:
+            while numero != 0:
+                print("1. Consultar Cantidad de Millas")
+                print("2. Acumular Millas")
+                print("3. Canjear Millas")
+                print("4. Elegir Otro Viajero")
+                print("5: Viajeros con mayor millas")
+                print("6. Salir")
+                opcion = int(input())
+                if opcion == 1:
+                    print(Viajero.cantidadTotalMillas(unviajero))
+                elif opcion == 2:
+                    millas = int(input("Ingrese la cantidad de millas a sumar"))
+                    print(Viajero.__add__(unviajero, millas))
+                elif opcion == 3:
+                    canjear = int(input("Ingrese la cantidaad de millas a canjear: "))
+                    print(Viajero.__sub__(unviajero, canjear))
+                elif opcion == 4:
+                    numero = 0
+                elif opcion == 5:
+                    maximo = mv.mayores()
+                    print(f"Los viajeros con mayor millas son: ")
+                    print(str(mv.mostarMayores(maximo)))
+                elif opcion == 6:
+                    exit()
+                else:
+                    print("----Opcion Invalida----")
+        numero = int(input("Ingrese el numero de un viajero: "))
